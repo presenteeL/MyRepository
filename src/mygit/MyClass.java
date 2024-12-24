3373c884f5e53687c336e227727e9236f0c3d9cb
presenteeL <leejh3940@skuniv.ac.kr>
2024-12-24 15:45:55 +0900

    df

--- src/mygit/MyClass.java
+++ src/mygit/MyClass.java
@@ -6,7 +6,7 @@ public class MyClass {
 		System.out.println("Hello Git!");
 		System.out.println("Hello Git!");
 		System.out.println("Hello Git!");
-		System.out.println("Hello Git!");
+		System.out.println("Hello Git!s");
 	}
 
 }
