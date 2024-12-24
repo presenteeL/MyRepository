4d3e37c86cae48e10a6549d978d10b52759d3d18
presenteeL <leejh3940@skuniv.ac.kr>
2024-12-24 16:11:56 +0900

    class

---
+++ src/mygit/B.java
@@ -0,0 +1,10 @@
+package mygit;
+
+public class B {
+	public void bb() {
+		int b1=1;
+		int b2=2;
+		
+		System.out.println(b1+b2);
+	}
+}
---
+++ src/mygit/C.java
@@ -0,0 +1,10 @@
+package mygit;
+
+public class C {
+	public void cc() {
+		int c1=1;
+		int c2=2;
+		
+		System.out.println(c1+c2);
+	}
+}
