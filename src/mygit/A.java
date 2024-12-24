cb5ed280a80464d9e8362407077623956cc0a238
presenteeL <leejh3940@skuniv.ac.kr>
2024-12-24 16:14:41 +0900

    devB push

--- src/mygit/A.java
+++ src/mygit/A.java
@@ -1,9 +1,9 @@
 package mygit;
 
-public class MyClass {
+public class A {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
+		System.out.println("Hellos Git!");
 		System.out.println("Hello Git!");
 		System.out.println("Hello Git!");
 		System.out.println("Hello Git!s");
--- src/mygit/B.java
+++ src/mygit/B.java
@@ -4,7 +4,8 @@ public class B {
 	public void bb() {
 		int b1=1;
 		int b2=2;
+		int b3=3;
 		
-		System.out.println(b1+b2);
+		System.out.println(b1+b2+b3);
 	}
 }
