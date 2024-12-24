d0716371c70a87851465648855d3de58b68a2e41
presenteeL <leejh3940@skuniv.ac.kr>
2024-12-24 16:34:46 +0900

    dfefer

--- src/mygit/B.java
+++ src/mygit/B.java
@@ -3,9 +3,9 @@ package mygit;
 public class B {
 	public void bb() {
 		int b1=1;
-		int b2=2;
+		int b32=2;
 		int b4=3;
 		
-		System.out.println(b1+b2+b4);
+		System.out.println(b1+b32+b4);
 	}
 }
--- src/mygit/C.java
+++ src/mygit/C.java
@@ -3,8 +3,8 @@ package mygit;
 public class C {
 	public void cc() {
 		int c1=1;
-		int c2=2;
+		int c32=2;
 		
-		System.out.println(c1+c2);
+		System.out.println(c1+c32);
 	}
 }
