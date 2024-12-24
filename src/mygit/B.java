ad6c19a235c730e3f6f30d1384f0fb994cf4853b
presenteeL <leejh3940@skuniv.ac.kr>
2024-12-24 16:45:56 +0900

    devB#

--- src/mygit/B.java
+++ src/mygit/B.java
@@ -3,9 +3,10 @@ package mygit;
 public class B {
 	public void bb() {
 		int b1=1;
+		int b_2=23;
 		int b34=3;
 		int b4=3; 
 		
-		System.out.println(b1+b34+b4);
+		System.out.println(b_2+b34+b4);
 	}
 }
